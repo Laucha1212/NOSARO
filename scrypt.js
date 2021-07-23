4c46c33f86f2d778bd1cf500bac55af7f8f7ba24
Lautaro Perez <lautaroperezga@gmail.com>
2021-07-22 21:05:26 -0300

    actualizado
    actualizado

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+function verMas() {
+    document.getElementById('mas').style.display = 'block';
+    document.getElementById('verMas').style.display = 'none';
+}
+function verMenos() {
+    document.getElementById('mas').style.display = 'none';
+    document.getElementById('verMas').style.display = 'block';
+}
