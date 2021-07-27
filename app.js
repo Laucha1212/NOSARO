410eacbb3a64a8c4f9b231f657149fa420b02b2d
Lautaro Perez <lautaroperezga@gmail.com>
2021-07-27 04:06:19 -0300

    verMas
    se arreglo la funcion de verMas

--- app.js
+++ app.js
@@ -121,7 +121,9 @@ particlesJS('particles-js',
 
 function verMas(){
   document.getElementById('mas').style.display = 'block';
+  document.getElementById('verMas').style.display = 'none';
   }
-  function verMenos(){
-    document.getElementById('mas').style.display = 'none';
-    }
+function verMenos(){
+  document.getElementById('mas').style.display = 'none';
+  document.getElementById('verMas').style.display = 'block';
+  }
