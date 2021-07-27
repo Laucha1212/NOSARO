ce91d7b7d5ffa8e2900e9a31bd91375fca2bbcde
Lautaro Perez <lautaroperezga@gmail.com>
2021-07-27 03:29:27 -0300

    PARTICLES -VerMas()

--- scrypt.js
+++
@@ -1,8 +0,0 @@
-function verMas() {
-    document.getElementById('mas').style.display = 'block';
-    document.getElementById('verMas').style.display = 'none';
-}
-function verMenos() {
-    document.getElementById('mas').style.display = 'none';
-    document.getElementById('verMas').style.display = 'block';
-}
